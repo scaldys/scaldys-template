16d9632e6f00f3d66098bcb4c445eb0c6fd2771f
Scaldys <scaldys@scaldys.net>
2026-04-29 19:47:09 +0200

    Add ASCII art header to CLI and refactor main callback:
    - Introduced `HeaderGroup` to prepend ASCII art to CLI help output.
    - Refactored `main` callback for cleaner implementation.
    - Added `art` library dependency (`>=6.5`) to support ASCII art functionality.

--- src/scaldys/cli/cli.py
+++ src/scaldys/cli/cli.py
@@ -2,6 +2,9 @@
 # cython: language_level=3
 
 import typer
+from typer.core import TyperGroup
+from click import Context
+from art import text2art, tprint
 
 import scaldys.cli.commands.cmd_export as cmd_export
 import scaldys.cli.commands.cmd_settings as cmd_settings
@@ -17,8 +20,26 @@ def version_callback(value: bool):
         raise typer.Exit()
 
 
+class HeaderGroup(TyperGroup):
+    def get_help(self, ctx: Context) -> str:
+        # Override get_help to prepend the ASCII art
+        return text2art(APP_NAME) + "\n" + super().get_help(ctx)
+
+
+def main(
+    ctx: typer.Context,
+    version: bool = typer.Option(None, "--version", callback=version_callback),
+):
+    """
+    Show the app's version number and exit.
+    """
+    pass
+
+
 app = typer.Typer(
     help=f"A CLI to run {APP_NAME} in a terminal window.",
+    cls=HeaderGroup,
+    callback=main,
     no_args_is_help=True,
     add_completion=False,
     rich_markup_mode=None,
@@ -29,16 +50,6 @@ app = typer.Typer(
 
 app.command()(cmd_export.export)
 app.add_typer(cmd_settings.app, name="settings")
-
-
-@app.callback()
-def main(
-    ctx: typer.Context,
-    version: bool = typer.Option(None, "--version", callback=version_callback),
-):
-    """
-    Show the app's version number and exit.
-    """
 
 
 if __name__ == "__main__":
