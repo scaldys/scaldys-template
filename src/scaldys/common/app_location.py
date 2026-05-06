355ac1293a6b31f61c6871959a03af5ec7ecb02b
Scaldys <scaldys@scaldys.net>
2026-05-06 08:17:58 +0200

    Replace `assert` statements with `RuntimeError` for environment variable checks in `app_location.py`.

--- src/scaldys/common/app_location.py
+++ src/scaldys/common/app_location.py
@@ -98,15 +98,15 @@ def get_os_app_data_path() -> Path:
 
     if platform.system() == "Windows":
         app_data_dir = os.getenv("LOCALAPPDATA")
-        # os.getenv() can return None; LOCALAPPDATA is a Windows system variable and shall exist (so PyRight is happy).
-        assert app_data_dir is not None
+        if app_data_dir is None:
+            raise RuntimeError("LOCALAPPDATA environment variable is not set")
         app_data_path = Path(app_data_dir, ORGANIZATION_NAME, APP_NAME)
     elif platform.system() == "Darwin":
         app_data_path = Path(dirs.user_data_dir)
     else:
         app_data_dir = os.getenv("HOME")
-        # os.getenv() can return None; HOME is a system variable and shall exist (so PyRight is happy).
-        assert app_data_dir is not None
+        if app_data_dir is None:
+            raise RuntimeError("HOME environment variable is not set")
         app_data_path = Path(app_data_dir, f".{APP_NAME}")
 
     return app_data_path
